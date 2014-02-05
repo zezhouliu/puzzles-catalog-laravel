43246e29e3609084295ff324b975536e8d47a6f9
iahs <iahs@users.noreply.github.com>
2014-02-05 16:15:03 -0500

    course details view

--- app/views/courses/show.blade.php
+++ app/views/courses/show.blade.php
@@ -1,8 +1,67 @@
 <h1>{{{ $course->title }}}</h1>
 
 <hr/>
-<h3>Description</h3>
-{{{ $course->description  }}}
+        <div class="form-group">
+            <strong>Title:</strong>
+            {{{$course->title}}}
+        </div>
 
-<hr>
-<p>...more info...</p>
+        <div class="form-group">
+            <strong>Cat num:</strong>
+            {{{$course->cat_num}}}
+        </div>
+
+        <div class="form-group">
+            <strong>Term:</strong>
+            {{{$course->term}}}
+        </div>
+
+        <div class="form-group">
+            <strong>Bracketed:</strong>
+            {{{$course->bracketed}}}
+        </div>
+
+        <div class="form-group">
+            <strong>Field:</strong>
+            {{{$course->field}}}
+        </div>
+
+        <div class="form-group">
+            <strong>Number:</strong>
+            {{{$course->number}}}
+        </div>
+
+        <div class="form-group">
+            <strong>Faculty:</strong>
+            {{{$course->faculty}}}
+        </div>
+
+        <div class="form-group">
+            <strong>Description:</strong>
+            {{ Form::textarea('description', null, array('class'=>'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            <strong>Prerequisites:</strong>
+            {{ Form::textarea('prerequisites', null, array('class'=>'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            <strong>Notes:</strong>
+            {{ Form::textarea('notes', null, array('class'=>'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            <strong>Meetings:</strong>
+            {{ Form::textarea('meetings', null, array('class'=>'form-control')) }}
+        </div>
+
+        <div class="form-group">
+            <strong>Building:</strong>
+            {{{$course->building}}}
+        </div>
+
+        <div class="form-group">
+            <strong>Room:</strong>
+            {{{$course->room}}}
+        </div>
