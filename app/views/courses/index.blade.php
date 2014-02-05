c1fc0ba5271d8e8a982926bb751e996012a2cd6f
Martin Ringdal <mringdal@gmail.com>
2014-02-05 15:43:13 -0500

    Add delete to faculty index and fix courses index

--- app/views/courses/index.blade.php
+++ app/views/courses/index.blade.php
@@ -1,15 +1,13 @@
 <div class="row">
 	@foreach($courses as $course)
-
 	    <div class="col-lg-12">
 	        <h1>{{{ $course->title }}}</h1>
-	            <a class="btn btn-primary" href="/courses/{{ $course->id }}" role="button">View</a>
-	            <a class="btn btn-primary" href="/courses/{{ $course->id }}/edit" role="button">Edit</a>
-	            {{ Form::open(array('route' => array('courses.destroy', $course->id), 'method' => 'delete', 'style'=>"display:inline")) }}
-                    <button type="submit" href="{{ URL::route('courses.destroy', $course->id) }}" class="btn btn-danger btn-mini">Delete</button>
-                {{ Form::close() }}
+        	{{ link_to_route('courses.show', 'View', $course->id, array('class'=>'btn btn-primary')) }}
+        	{{ link_to_route('courses.edit', 'Edit', $course->id, array('class'=>'btn btn-primary')) }}
+            {{ Form::open(array('route' => array('courses.destroy', $course->id), 'method' => 'delete', 'style'=>"display:inline")) }}
+                <button type="submit" href="{{ URL::route('courses.destroy', $course->id) }}" class="btn btn-danger btn-mini">Delete</button>
+            {{ Form::close() }}
 	    </div>
-
 	@endforeach
 
 	{{$courses->links()}}
