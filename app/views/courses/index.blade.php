06770fcaa15f6a8e02bae1d993ce90c87bf95b21
Martin Ringdal <mringdal@gmail.com>
2014-02-05 15:30:10 -0500

    Add delete buttons to the courses index view

--- app/views/courses/index.blade.php
+++ app/views/courses/index.blade.php
@@ -3,10 +3,11 @@
 
 	    <div class="col-lg-12">
 	        <h1>{{{ $course->title }}}</h1>
-	        <p>
 	            <a class="btn btn-primary" href="/courses/{{ $course->id }}" role="button">View</a>
 	            <a class="btn btn-primary" href="/courses/{{ $course->id }}/edit" role="button">Edit</a>
-	        </p>
+	            {{ Form::open(array('route' => array('courses.destroy', $course->id), 'method' => 'delete', 'style'=>"display:inline")) }}
+                    <button type="submit" href="{{ URL::route('courses.destroy', $course->id) }}" class="btn btn-danger btn-mini">Delete</button>
+                {{ Form::close() }}
 	    </div>
 
 	@endforeach
