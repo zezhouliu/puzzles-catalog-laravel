c08cb857117d08136508d13fe8abaeca6401a4c7
iahs <iahs@users.noreply.github.com>
2014-02-05 15:47:30 -0500

    adding database migration to decrease course record size

--- app/database/migrations/2014_02_05_201853_streamline_courses.php
+++ app/database/migrations/2014_02_05_201853_streamline_courses.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class StreamlineCourses extends Migration
 {
@@ -11,8 +12,10 @@ class StreamlineCourses extends Migration
      */
     public function up()
     {
+        Schema::table('courses', function (Blueprint $table) {
         DB::query("ALTER TABLE  `courses` CHANGE  `term`  `term` ENUM(  'FALL',  'SPRING' ) ");
         DB::query("ALTER TABLE  `courses` CHANGE  `cat_num`  `cat_num` INT( 5 ) UNSIGNED NOT NULL");
+        });
     }
 
     /**
@@ -22,8 +25,9 @@ class StreamlineCourses extends Migration
      */
     public function down()
     {
-        DB::query("ALTER TABLE  `courses` CHANGE  `term`  `term` varchar ");
-        DB::query("ALTER TABLE  `courses` CHANGE  `cat_num`  `cat_num` INT( 11 ) NOT NULL");
+            DB::query("ALTER TABLE  `courses` CHANGE  `term`  `term` varchar ");
+            DB::query("ALTER TABLE  `courses` CHANGE  `cat_num`  `cat_num` INT( 11 ) NOT NULL");
+        });
     }
 
 }
