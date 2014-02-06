3dcda7d384cc057910c602d437d40e7fcc7e110f
Zezhou Liu <zezhouliu@college.harvard.edu>
2014-02-05 16:59:39 -0800

    Updated the index for fields

--- app/views/fields/index.blade.php
+++ app/views/fields/index.blade.php
@@ -1,14 +1,26 @@
-<div class="row">
-	@foreach($fields as $field)
+<h1>List of courses</h1>
+<table class="table">
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Code</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($fields as $field)
+			<tr>
+			<td>{{{ $field->name }}}</td>
+			<td>{{{ $field->code }}}</td>
+			<th>
+				{{ link_to_route('fields.show', 'View', $field->id, array('class'=>'btn btn-primary')) }}
+    			{{ link_to_route('fields.edit', 'Edit', $field->id, array('class'=>'btn btn-primary')) }}
+				{{ Form::open(array('route' => array('fields.destroy', $field->id), 'method' => 'delete', 'style'=>"display:inline")) }}
+            		<button type="submit" href="{{ URL::route('fields.destroy', $field->id) }}" class="btn btn-danger btn-mini">Delete</button>
+        		{{ Form::close() }}
+			</th>
+			</tr>
+		@endforeach
+	</tbody>
 
-	    <div class="col-lg-12">
-	        <h1>{{{ $field->title }}}</h1>
-	        <p>
-	            <a class="btn btn-primary" href="/fields/{{ $field->id }}" role="button">View</a>
-	        </p>
-	    </div>
+</table>
 
-	@endforeach
-
-	{{$fields->links()}}
-</div>
